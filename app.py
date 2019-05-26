230080fea823bbbda20d78180e10178c7b300854
jacky chen <jc8121@nyu.edu>
2019-05-26 19:34:36 -0400

    '2'

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 from flask import Flask, render_template
+
+response = unirest.post("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
+  headers={
+    "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    "X-RapidAPI-Key": "1c3b759866msh08fc8fcca0665b0p1efcfdjsn3fe94d77020c",
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  params={
+    "inboundDate": "2019-09-10",
+    "cabinClass": "business",
+    "children": 0,
+    "infants": 0,
+    "country": "US",
+    "currency": "USD",
+    "locale": "en-US",
+    "originPlace": "SFO-sky",
+    "destinationPlace": "LHR-sky",
+    "outboundDate": "2019-09-01",
+    "adults": 1
+  }
+)
+
+
 app = Flask(__name__)
 
 posts = [
@@ -11,7 +34,8 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def hello():
-	return render_template('home.html', posts=posts)
+	return response
+	#render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
